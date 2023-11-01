6420aaffbfa444110a9ee5eabfa54aae2d7dc6c2
Alan <alan2000129@gmail.com>
2023-10-31 22:02:38 -0400

    Turn off the login/signup component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+//import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+//import Login from "./components/Login";
+//import Signup from "./components/Signup";
 
 function App() {
-	const user = localStorage.getItem("token");
+	//const user = localStorage.getItem("token");
 
 	return (
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-		</Routes>
+		<Main />
 	);
 }
 
